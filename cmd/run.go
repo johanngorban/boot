b907aaca53bf472d0f34c5867aee4aabf776887e
der-johanni <ivangorbanwork@gmail.com>
2026-07-18 22:05:49 +0500

    feat: run enpoint implemented

---
+++ cmd/run.go
@@ -0,0 +1,63 @@
+package cmd
+
+import (
+	"fmt"
+	"stm32-bootctl/pkg/bcp"
+
+	"github.com/spf13/cobra"
+)
+
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "",
+	Run:   wrap(run),
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+	runCmd.Flags().Uint("slot", 1, "Slot number")
+	runCmd.MarkFlagRequired("slot")
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	jsonEnabled, _ := cmd.Flags().GetBool("json")
+	port, _ := cmd.Flags().GetString("port")
+	baudrate, _ := cmd.Flags().GetUint("baud")
+	slot, _ := cmd.Flags().GetUint("slot")
+
+	c, err := bcp.Open(port, uint16(baudrate))
+	if err != nil {
+		return fmt.Errorf("open port %s: %w", port, err)
+	}
+	defer c.Close()
+
+	req, err := bcp.NewRequest(bcp.VersionCommand, nil)
+	if err != nil {
+		return fmt.Errorf("build request: %w", err)
+	}
+
+	if err = c.Send(req); err != nil {
+		return fmt.Errorf("send: %w", err)
+	}
+
+	resp, err := c.Recv()
+	if err != nil {
+		return fmt.Errorf("recv: %w", err)
+	}
+
+	if resp.Command != req.Command {
+		return fmt.Errorf("unexpected command in response: 0x%02X", uint8(resp.Command))
+	}
+
+	if resp.IsOk() {
+		if jsonEnabled {
+			fmt.Printf("slot #%d starting", slot)
+		} else {
+			fmt.Printf("Starting slot #%d\n", slot)
+		}
+	} else {
+		return fmt.Errorf("version fetching failed: %s", resp.StatusName())
+	}
+
+	return nil
+}
